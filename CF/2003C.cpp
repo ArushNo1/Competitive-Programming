dfbe1c4afe44d168fea1f013dddfef8f6bdf17b8
ArushNo1 <arushbodla@gmail.com>
2024-08-25 16:54:14 -0400

    Codeforces round 968

---
+++ CF/2003C.cpp
@@ -0,0 +1,141 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops") 
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef vector<bool> vb;
+
+typedef tree<int,null_type,less<int>,rb_tree_tag,
+tree_order_statistics_node_update> indexed_set;
+
+
+#define endll '\n'
+
+#define all(x) (x).begin(), (x).end()
+#define MOD ll(1e9+7)
+#define INF ll(2e63-1)
+#define EPS ld(1e-9)
+#define dbg(x) cerr << __LINE__ << ": " << #x << "=" << x << endll;
+#define print(x) cout << __LINE__ << ": " << #x << "=" << x << endll;
+
+template <typename T>
+ostream& operator<< (ostream& os, const vector<T>& arr){
+	os << "[";
+    for(const T x : arr){
+		os << x << " ";
+	}
+	os << "]";
+    return os;
+}
+
+template <typename T, typename U>
+ostream& operator<< (ostream& os, const pair<T, U>& x){
+    os << "(" << x.first << ", " << x.second << ")";
+    return os;
+}
+
+template <typename T, typename U>
+ostream& operator<< (ostream& os, const vector<pair<T, U>>& arr){
+	os << "{\n";
+    for(const pair<T,U>& x : arr){
+		os << "    " << x << endll;
+	}
+	os << "}";
+    return os;
+}
+
+template <typename T, typename U>
+ostream& operator<< (ostream& os, const map<T, U>& arr){
+	os << "{\n";
+    for(const pair<T,U>& x : arr){
+		os << "    " << x << endll;
+	}
+	os << "}";
+    return os;
+}
+
+template <typename T>
+inline void fillv(vector<T>& v, int n) {
+    for (int i = 0; i < n; ++i) {
+        std::cin >> v[i];
+    }
+}
+template <typename T, typename U>
+inline void fillv(vector<pair<T, U>>& v, int n, bool inv = false){
+	if(inv){
+		for(int i = 0 ; i < n; ++i){
+			cin >> v[i].second >> v[i].first;
+		}
+		return;
+	}
+	for (int i = 0; i < n; ++i) {
+        cin >> v[i].first >> v[i].second;
+    }
+	return;
+}
+
+inline void open(string name){
+    freopen((name + ".in").c_str(), "r", stdin);
+	freopen((name + ".out").c_str(), "w", stdout);
+}
+
+
+void solve(int num_tc)
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    string res = "";
+
+    vi freq(26);
+    for(int i = 0; i < n; i++){
+        freq[s[i] - 'a']++;
+    }
+    priority_queue<pair<int, char>> heap;
+    for(int i = 0; i < 26; i++){
+        if(freq[i] > 0){
+            heap.push({freq[i], i + 'a'});
+        }
+    }
+    pair<int, char> prev = {-1, '.'};
+    while(!heap.empty()){
+        pair<int, char> c = heap.top();
+        heap.pop();
+        res += c.second;
+        c.first--;
+        if(prev.first > 0){
+            heap.push(prev);
+        }
+        prev = c;
+    }
+    for(int i = 0; i < prev.first; i++){
+        res += prev.second;
+    }
+    cout << res << endll;
+    freq.clear();
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);  
+
+    ll T = 1;
+    cin >> T;
+    for(ll t = 0; t < T; t++){
+        solve(t+1);
+    }
+}
