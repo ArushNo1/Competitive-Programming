6c077090bb1cadb75ccd80a5b4ef768d1470771f
Arush Bodla <arushbodla@gmail.com>
2025-12-15 21:14:49 -0500

    ACSL 2025 Folder

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.println(Math.sqrt(30));
+        System.out.println(Math.sin(100));
+        System.out.println(Math.cos(100));;
+        System.out.println(Math.round(44.7));
+        System.out.println(Math.floor(44.7));
+        System.out.println(Math.ceil(44.7));
+        System.out.println(Math.min('K', 70));
+        System.out.println(Math.max('K', 70));
+        System.out.println(Math.pow(7, 8));
+        System.out.println(
+            Math.PI * 3 * Math.sin(Math.toRadians(187)) + Math.abs(Math.cos(Math.toRadians(122)))
+        );
+        }
+}
